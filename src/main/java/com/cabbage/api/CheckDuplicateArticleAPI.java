074afedb6f5c6f1af64304b21ff51597ca214bd2
Tung Nguyen <tungntd00605@fpt.edu.vn>
2019-08-03 08:48:25 +0700

    sửa response của api check duplicate article

--- src/main/java/com/cabbage/api/CheckDuplicateArticleAPI.java
+++ src/main/java/com/cabbage/api/CheckDuplicateArticleAPI.java
@@ -1,12 +1,14 @@
 package com.cabbage.api;
 
 import com.cabbage.entity.Article;
+import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
@@ -17,10 +19,16 @@ public class CheckDuplicateArticleAPI extends HttpServlet {
         Article article = ofy().load().type(Article.class).filter("url", url).first().now();
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
+        HashMap<String, String> responseObject = new HashMap<>();
+
         if(article == null){
-            resp.getWriter().println("no-duplicate");
+            responseObject.put("message", "This URL is not duplicated");
+            responseObject.put("status", "0");
+            resp.getWriter().println(new Gson().toJson(responseObject));
         } else {
-            resp.getWriter().println("duplicate");
+            responseObject.put("message", "This URL is duplicated");
+            responseObject.put("status", "1");
+            resp.getWriter().println(new Gson().toJson(responseObject));
         }
     }
 }
